85ddc26e2b5567ef61622e29780e2ae08457516c
Eshan <eshansalwan@gmail.com>
2022-07-26 01:17:31 -0400

    Basic Navigation drawer skeleton made

--- app/src/main/java/eshan/salwan/n01422232/EshanActivity.java
+++ app/src/main/java/eshan/salwan/n01422232/EshanActivity.java
@@ -1,10 +1,11 @@
+//Eshan Salwan, N01422232, RNB
 package eshan.salwan.n01422232;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EshanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
