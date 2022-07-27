b0f3356d13def20a16da075e3deb84bf874f99bf
Eshan <eshansalwan@gmail.com>
2022-07-27 01:47:47 -0400

    Working Navigation drawer skeleton complete

--- app/src/main/java/eshan/salwan/n01422232/EshanActivity.java
+++ app/src/main/java/eshan/salwan/n01422232/EshanActivity.java
@@ -1,15 +1,82 @@
 //Eshan Salwan, N01422232, RNB
 package eshan.salwan.n01422232;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class EshanActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class EshanActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    //FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 6 - Configure all views
+        configureToolBar();
+        configureDrawerLayout();
+        configureNavigationView();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // 5 - Handle back click to close menu
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void configureNavigationView() {
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void configureDrawerLayout() {
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureToolBar() {
+        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.home:
+                break;
+            case R.id.download:
+                break;
+            case R.id.weather:
+                break;
+            case R.id.fileContent:
+                break;
+            case R.id.settings:
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
